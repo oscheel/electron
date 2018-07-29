01b6b047056395176d5df0b9fff88a6b15923f5a
oscheel <ottoscheel@gmail.com>
2018-07-29 14:56:44 -0400

    Cleaning code

---
+++ app/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import View from './app/layout/View/View'
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+    <View title={"myTitle"}/>,
+    document.getElementById('root')
+)
